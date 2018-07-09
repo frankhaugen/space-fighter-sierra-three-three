ef981165ca548edd9078b3aab90367c7ba8fc877
frank <frank.haugen@gmail.com>
2018-07-09 16:08:02 +0200

    Created test elements

---
+++ Assets/Testing/FireBlaster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBlaster : MonoBehaviour {
+
+    public ParticleSystem blasterParent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            ParticleSystem blaster = new ParticleSystem();
+
+            blaster = blasterParent;
+        }
+	}
+}
