664ae9a48a2fb032002800d124160a51522eaee4
frank <frank.haugen@gmail.com>
2018-07-12 20:58:11 +0200

    Edited rotate script

--- Assets/Testing/Rotate.cs
+++ Assets/Testing/Rotate.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour {
 
+    DeadZone deadZone = new DeadZone();
+    public float HorizontalInput;
+    public float VerticalInput;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        HorizontalInput = deadZone.CheckIfInDeadZone(Input.GetAxis("Horizontal"));
+        VerticalInput = deadZone.CheckIfInDeadZone(Input.GetAxis("Vertical"));
+    }
+
+    void ChangeRotation()
+    {
+        transform.Rotate(Vector3.up * TurnSpeedMultiplyer * Time.deltaTime * -1);
+    }
 }
